f48625c971f51fe977d5d486925fe047d1843c60
lethanh-dev <lethanh.1701.dev@gmail.com>
2024-09-27 16:48:26 +0700

    add

--- BE/index.js
+++ BE/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(`mongodb://localhost:27017/crypto-bot`, {
+mongoose.connect('mongodb://LeThanh88:LeThanh88@localhost:27017/crypto-bot', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
